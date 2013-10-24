c98c0b762b83b36315824f7bc679c25ca5d7a100
Aleksy Jones <aleksy.jones@gmail.com>
2013-10-23 20:20:33 -0400

    Node server correctly functioning as a caching static server for now.

--- node_server/node_server.js
+++ node_server/node_server.js
@@ -1,14 +1,23 @@
 // main server node
 var http = require('http');
 var url = require('url') ;
+var path = require("path");
 var mustache = require("mustache");
 var fs = require('fs') ;
-var kHomeDir = './';
-var kStaticFilesDir = ""; 
+var kDir = 
+{
+    static_files : ["../aleksy_sandbox/", "../website/"],
+    home_dir : "./node_server/"
+};
 var kIsDoingFileUpdate = false;
-var gReadableStreams = new Object();
-var gIgnoreList = new Object();
-var gCachedFiles = new Object();
+
+var kPortToBind = 8088;
+//used for c9.io
+if (process.env.PORT !== undefined ) kPortToBind = process.env.PORT;
+
+var gReadableStreams = {};
+var gIgnoreList = {};
+var gCachedFiles = {};
 var gQueuedFileUpdates = 0;
 var gFilesRead = 0;
 var gFilesSkipped = 0;
@@ -21,64 +30,37 @@ function ProcessQuery(query, value)
 		case 'refresh':
 			StaticFileCacheRefresh(RepeatedFileRefresh);
 			return "Initiated Server file Refresh";
-		case 'interpret':
-
 		default:
 			return "unrecognized query:";
 	}
 }
-function GetHtmlHeader()
+
+function GetCachedFile(relative_path)
 {
-	return '<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\" dir=\"ltr\">\n';
-}
-function GetHtmlFooter()
-{
-	return '</html>';
-}
-
-
-function WriteD3CodeThing(res, template, queries,  when_done)
-{
-
-	view = 
-	{
-		title: "D3 tutorial",
-		css_files :
-		[
-			{path: "syntaxhighlighter/styles/shCoreMidnight.css", media: "screen" },
-			{path: "syntaxhighlighter/styles/shCore.css", media: "screen" }
-		],
-
-		script_files :
-		[
-			{path: 'misc_scripts.js'},
-			{path: 'd3/d3.v3.js'},
-			{path: 'syntaxhighlighter/scripts/shCore.js'},
-			{path: 'syntaxhighlighter/scripts/shBrushCpp.js'}
-		]
+    
+    if (relative_path == '/') relative_path = "index.html";
+    var fullpath = "";
+    for(var i=0; i < kDir.static_files.length; ++i)
+    {
+        fullpath = path.normalize( kDir.home_dir + kDir.static_files[i]+relative_path);
+        console.log("looking for full path: " + fullpath);
+        if(gCachedFiles[fullpath])
+        {
+            return gCachedFiles[fullpath];
+        }
 	}
-	if(queries['source_file'] !== undefined) console.log(queries['source_file']);
-	if(queries['source_file'] !== undefined && gCachedFiles['./'+queries['source_file']] !== undefined)
-	{
-		view.json_data = [];
-		var line_strings = gCachedFiles['./'+queries['source_file']].buffer.
-				toString().replace(/\\/g, "\u005c\u005c" ).replace(/"/g, '\u005c\u0022').replace(/'/g, '\u005c\u0027').replace(/\t/g, '    ').split('\n');//replace(/</g,"&lt;").replace(/>/g, '&gt;')
-		for (var i = line_strings.length - 1; i >= 0; i--) {
-			view.json_data.push( {line_string: line_strings[i]} );
-		};
-	}
-
-	res.write(mustache.to_html(template, view));
-	when_done();
 }
 
 var requestListener = function (req, res) {
 	var parsed = url.parse(req.url,true);
-	var path = "." + parsed.pathname;
-	console.log("." + parsed.pathname);
+	var path = parsed.pathname;
+	console.log(parsed.pathname);
 	var queries = parsed.query;
-	var mu_file = path.replace(/\.html$/, ".mu.html");
-	if(gCachedFiles[path] !== undefined && gCachedFiles[path].buffered )
+	//console.log(req);
+    
+    var cached_file = GetCachedFile(path);
+    
+	if(cached_file !== undefined && cached_file.buffered )
 	{
 		console.log("found!");
 		if(queries['interpreter'] !== undefined)
@@ -86,16 +68,9 @@ var requestListener = function (req, res) {
 		}
 
 		res.writeHead(200);
-		res.write(gCachedFiles[path].buffer);
+		res.write(cached_file.buffer);
 		res.end();
 		
-	}
-	else if(gCachedFiles[mu_file] !== undefined && gCachedFiles[mu_file].buffered )
-	{
-		console.log(mu_file);
-		res.writeHead(200);
-		WriteD3CodeThing(res, gCachedFiles[mu_file].buffer.toString(), queries, function()
-		{res.end();} );
 	}
 	else{
 		console.log("not found!");
@@ -112,31 +87,39 @@ var requestListener = function (req, res) {
 
 function RefreshFile(err, relative_path, stat, dir_prefix, doneRefreshCallBack)
 {
+	if(stat === undefined)
+	{
+        console.log(err);
+        console.log(dir_prefix+relative_path);
+        gQueuedFileUpdates--;
+        return;
+	}
 	if(stat.isFile() && !stat.isDirectory())
 	{
 		var read_file = dir_prefix+relative_path;
-		var index = dir_prefix+relative_path;
-		if(gCachedFiles[index] == undefined || gCachedFiles[index].stat == undefined || gCachedFiles[index].stat.mtime < stat.mtime )
+		var index = path.normalize( dir_prefix+relative_path);
+		console.log("RefreshFile: " + index);
+		if(gCachedFiles[index] === undefined || gCachedFiles[index].stat === undefined || gCachedFiles[index].stat.mtime < stat.mtime )
 		{
-			gCachedFiles[index]  = new Object();
+			gCachedFiles[index]  = {};
 			gCachedFiles[index].stat = stat;
-			delete gCachedFiles[index].buffer;
-			gReadableStreams[index] = fs.createReadStream(dir_prefix+relative_path);
+			gCachedFiles[index].buffer = new Buffer(0);
+			gReadableStreams[index] = fs.createReadStream(index);
 			gReadableStreams[index].on('data', function(chunk) {
-				if(gCachedFiles[index].buffer == undefined){
+				if(gCachedFiles[index].buffer === undefined){
 					gCachedFiles[index].buffered = true;
 					gCachedFiles[index].buffer = chunk;
 				}
 				else gCachedFiles[index].buffer = Buffer.concat([gCachedFiles[index].buffer, chunk]);
-				//console.log('got %d bytes of data from ' + dir_prefix+relative_path, chunk.length);
+				console.log('got %d bytes of data from ' + index, chunk.length);
 			});
-			gReadableStreams[dir_prefix+relative_path].on('end', function() {
-				delete gReadableStreams[dir_prefix+relative_path];
+			gReadableStreams[index].on('end', function() {
+				delete gReadableStreams[index];
 				gCachedFiles[index].buffered = true;
 				gQueuedFileUpdates--;
 				gFilesRead++;
 				//console.log("stream close, counter = %d ", gQueuedFileUpdates);
-				if(gQueuedFileUpdates == 0) doneRefreshCallBack();
+				if(gQueuedFileUpdates === 0) doneRefreshCallBack();
 			});
 		}
 		else
@@ -144,7 +127,7 @@ function RefreshFile(err, relative_path, stat, dir_prefix, doneRefreshCallBack)
 			gQueuedFileUpdates--;
 			gFilesSkipped++;
 			//console.log("skipped update, counter = %d ", gQueuedFileUpdates);
-			if(gQueuedFileUpdates == 0) doneRefreshCallBack();
+			if(gQueuedFileUpdates === 0) doneRefreshCallBack();
 		}
 
 	}
@@ -162,7 +145,7 @@ function RefreshFile(err, relative_path, stat, dir_prefix, doneRefreshCallBack)
 		gQueuedFileUpdates--;
 		//console.log("else, counter = %d ", gQueuedFileUpdates); 
 		//console.log("Unknown file type " + relative_path);
-		if(gQueuedFileUpdates == 0) doneRefreshCallBack();
+		if(gQueuedFileUpdates === 0) doneRefreshCallBack();
 	}
 
 }
@@ -175,7 +158,7 @@ function ProcessFile(relative_path, dir_prefix, doneRefreshCallBack)
 		//console.log("process, counter = %d ", gQueuedFileUpdates);
 		var filename = relative_path;
 		var recursed_prefix = dir_prefix;
-			//console.log("files " + filename + " " + recursed_prefix );
+		console.log("files " + filename + " " + recursed_prefix );
 		fs.stat(recursed_prefix +filename, function(err, stat){ RefreshFile(err, filename, stat, recursed_prefix, doneRefreshCallBack);} );
 	}
 }
@@ -184,13 +167,14 @@ function RefreshDirectoryCache(err, files, dir_prefix, doneRefreshCallBack)
 {
 	//console.log("Refresh directory, counter = %d " + dir_prefix, gQueuedFileUpdates);
 	//console.log('callback: ' + callback);
+	if(files === undefined) return;
 	for (var i = files.length - 1; i >= 0; i--) 
 	{
 		ProcessFile(files[i], dir_prefix, doneRefreshCallBack);
 	};
 	gQueuedFileUpdates--;
-	//console.log("directory, counter = %d " + dir_prefix, gQueuedFileUpdates);
-	if(gQueuedFileUpdates == 0)
+	console.log("directory, counter = %d " + dir_prefix, gQueuedFileUpdates);
+	if(gQueuedFileUpdates === 0)
 	{
 		console.log('exit at refreshdir');
 		doneRefreshCallBack();
@@ -198,6 +182,12 @@ function RefreshDirectoryCache(err, files, dir_prefix, doneRefreshCallBack)
 
 }
 
+function ReadDir(home_dir, dir, doneRefreshCallBack)
+{
+    console.log("reading " + home_dir + dir);
+     fs.readdir(home_dir+dir, 
+        function(err, files){RefreshDirectoryCache(err, files, home_dir + dir, doneRefreshCallBack); });
+}
 
 function StaticFileCacheRefresh(doneRefreshCallBack)
 {
@@ -205,27 +195,18 @@ function StaticFileCacheRefresh(doneRefreshCallBack)
 	gFilesRead = 0;
 	gFilesSkipped = 0;
 	gQueuedFileUpdates++;
-	fs.readdir(kHomeDir+kStaticFilesDir, function(err, files) {RefreshDirectoryCache(err, files, kHomeDir +kStaticFilesDir, doneRefreshCallBack); });
+	for(var i=0; i < kDir.static_files.length; ++i)
+	{
+		ReadDir(kDir.home_dir, kDir.static_files[i], doneRefreshCallBack);
+	}
 }
 
 var server = http.createServer(requestListener);
 function BindServer()
 {
 	console.log("done initial update, now listening on port 80");
-	server.listen(8088);
-	if (process.getuid && process.setuid) {
-		console.log('Current uid: ' + process.getuid());
-		try {
-			process.setuid('aleksy');
-			console.log('New uid: ' + process.getuid());
-		}
-		catch (err) {
-			console.log('Failed to set uid: ' + err);
-		}
-	}
-
+	server.listen(kPortToBind);
 	RepeatedFileRefresh();
-	
 }
 
 function RepeatedFileRefresh()
@@ -245,9 +226,13 @@ function PopulateIgnoreList(buffer)
 	{
 		var strings = buffer.toString().split("\n");
 		console.log(strings);
+		var p = "";
 		for (var i = strings.length - 1; i >= 0; i--) {
-			gIgnoreList[strings[i]] = true;
-			console.log(strings[i]);
+			var result = strings[i].match(/\w*#.*/);
+			if (result !== undefined) continue;
+			p = path.normalize(strings[i]);
+			gIgnoreList[p] = true;
+			console.log(p);
 		};
 	}
 	console.log("ignoring: \n");
@@ -257,9 +242,10 @@ function PopulateIgnoreList(buffer)
 function OnBootup()
 {
 	gQueuedFileUpdates = 0;
-	ProcessFile(".nodeignore", "./", function()
-		{ 
-			PopulateIgnoreList(gCachedFiles['./.nodeignore'].buffer);
+	ProcessFile(".nodeignore", kDir.home_dir, function()
+		{
+            console.log(gCachedFiles);
+			PopulateIgnoreList(gCachedFiles[path.normalize(kDir.home_dir+'.nodeignore')].buffer);
 			StaticFileCacheRefresh(BindServer);
 		});
 }
