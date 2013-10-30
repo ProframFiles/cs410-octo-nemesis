d138ca38165ff8558f9a3896886635d771c71836
rdjeric <rdjeric@interchange.ubc.ca>
2013-10-29 22:17:20 -0700

    Spelling

--- website/Presenter/forestPresenter.js
+++ website/Presenter/forestPresenter.js
@@ -1,5 +1,5 @@
 require([/*the different .js files needed here (with path relative to entry path), in strings*/],
-	function(/*the order of the above classes, with wahtever coresponsing naem you'd like*/)){
+	function(/*the order of the above classes, with whatever corresponding name you'd like*/)){
 	// Private variables and private functions
 
 
