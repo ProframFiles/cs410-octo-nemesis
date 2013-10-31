564230686e0718297756b807a67e9ba5965cb574
Aleksy Jones <aleksy.jones@gmail.com>
2013-10-30 23:53:21 -0700

    Code analyzer is working, also ugly

--- code_analyser/src/analyzer.cpp
+++ code_analyser/src/analyzer.cpp
@@ -42,6 +42,7 @@
 
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/AST/CXXInheritance.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Lex/Lexer.h"
@@ -56,7 +57,7 @@
 #include "JSONReader.hpp"
 #include "JSONCallbacks.hpp"
 #include "CompilationDatabase.hpp"
-
+#include <unordered_map>
 
 
 
@@ -65,51 +66,135 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 using namespace llvm;
 
+
+
+
+struct ClassRecord
+{
+	std::string mUSR;
+	std::string mFullname;
+	std::string mName;
+	std::vector<size_t> mChildren;
+
+	void DumpToJSON(sjp::JSONObjectIO* json, const std::vector<ClassRecord>& parent_array) const
+	{
+		json->StartObject(mFullname);
+		json->AddValue(std::string("qualified name"), mFullname);
+		json->AddValue(std::string("name"), mName);
+		json->AddValue(std::string("USR"), mUSR);
+		json->StartArray(std::string("childArray"));
+		for (size_t i = 0; i < mChildren.size(); ++i)
+		{
+			json->AddValue( parent_array.at(mChildren.at(i)).mFullname );
+		}
+		json->EndCurrent();
+		json->EndCurrent();
+
+	}
+
+};
+
+
 namespace
 {
 class ToolTemplateCallback : public MatchFinder::MatchCallback
 {
 public:
-	ToolTemplateCallback(sjp::JSONObjectIO* json_out) : mJSON(json_out) {}
+	ToolTemplateCallback() 
+		: mClassRecords()
+	{mClassRecords.reserve(1024);}
 
 	virtual void run(const MatchFinder::MatchResult& Result)
 	{
-		const RecordDecl* CE = Result.Nodes.getNodeAs<RecordDecl>("statementer");
-		if(CE != NULL && (clang::SrcMgr::C_User == Result.SourceManager->getFileCharacteristic(CE->getLocStart())))
+		const CXXRecordDecl* CE = Result.Nodes.getNodeAs<CXXRecordDecl>("statementer");
+		
+		if(CE != NULL 
+			&& (clang::SrcMgr::C_User == Result.SourceManager->getFileCharacteristic(CE->getLocStart()))
+			&& !CE->isInjectedClassName()
+			&& CE->isCanonicalDecl())
 		{
-			const bool orig = CE->isCanonicalDecl();
-			StringRef name = CE->getName();
-			bool invalid = 0;
-			if(orig && !name.empty())
-			{
-				mJSON->StartObject(std::string(name.data()));
-				
-				const char* start = Result.SourceManager->getCharacterData(CE->getLocStart(), &invalid);
-				const char* end = Result.SourceManager->getCharacterData(CE->getLocEnd(), &invalid);
-
-				if(!invalid && (end > start) && (end - start) < 256)
-				{
-					mTmpString.clear();
-				
-					while(start <= end)
-					{
-						mTmpString.push_back(*start++);
-					}
-					mJSON->AddValue(std::string("source"), mTmpString);
-				}
-				clang::index::generateUSRForDecl(CE, mTmpUSR);
-				mJSON->AddValue(std::string("USR"), std::string(&mTmpUSR[0]));
-				mJSON->EndCurrent();
-			}
-			
+			ProcessRecordDecl(CE);
 		}
+	}
+
+	void ProcessRecordDecl(const CXXRecordDecl* CE)
+	{
+		for (size_t i = 0; i < mClassRecords.size(); i++)
+		{
+			CXXBasePaths paths;
+			if(CE->isDerivedFrom(mDecls.at(i), paths))
+			{
+				// are we immediately descended?
+				if(paths.begin()->size() < 2)
+				{
+					mClassRecords.at(i).mChildren.push_back(mClassRecords.size());
+				}
+			}
+		}
+		mDecls.push_back(CE);
+		mClassRecords.emplace_back();
+		ClassRecord& cr = mClassRecords.back();
+		mTmpSS.clear();
+		raw_svector_ostream sout(mTmpSS);
+		cr.mName = CE->getName();
+		CE->printQualifiedName(sout);
+		PrintTemplateParams(CE->getDescribedClassTemplate(), sout);
+		StringRef name = sout.str();
+		cr.mFullname = std::string(name.data(), name.size());
+
+		mTmpSS.clear();
+		
+		index::generateUSRForDecl(CE, mTmpSS);
+		cr.mUSR = std::string(&mTmpSS[0], mTmpSS.size());
+		mIndexMap.insert(std::make_pair(cr.mUSR, mClassRecords.size()-1));
+	}
+
+	void PrintTemplateParams(const ClassTemplateDecl*  TD, raw_svector_ostream& sout)
+	{
+		if(TD)
+		{
+			const TemplateParameterList* TP = TD->getTemplateParameters();
+			ArrayRef<const NamedDecl*> param_array = TP->asArray();
+			sout << '<';
+			for (size_t i = 0; i < param_array.size(); i++)
+			{
+				param_array[i]->printQualifiedName(sout);
+			}
+			sout << '>';
+		}
+	}
+
+	void PrintSource(const std::string& json_label, const CXXRecordDecl* CE,  const MatchFinder::MatchResult& Result)
+	{
+		bool invalid = false;
+		const char* start = Result.SourceManager->getCharacterData(CE->getLocStart(), &invalid);
+		const char* end = Result.SourceManager->getCharacterData(CE->getInnerLocStart(), &invalid);
+		mTmpString.clear();
+		if(!invalid && (end > start) && (end - start) < 1256)
+		{
+			while(start <= end)
+			{
+				mTmpString.push_back(*start++);
+			}
+		}
+	}
+
+	void DumpAllToJSON(sjp::JSONObjectIO* json) const
+	{
+		for (size_t i = 0; i < mClassRecords.size() ; i++)
+		{
+			mClassRecords.at(i).DumpToJSON(json, mClassRecords);
+		}
+		
 	}
 
 private:
 	
-	SmallVector<char, 1024> mTmpUSR;
+	std::unordered_map<std::string, size_t> mIndexMap;
+	std::vector<ClassRecord> mClassRecords;
+	std::vector<const CXXRecordDecl*> mDecls;
+	SmallVector<char, 1024> mTmpSS;
 	std::string mTmpString;
-	sjp::JSONObjectIO* mJSON;
 };
 
 
@@ -127,7 +212,7 @@ cl::list<std::string> SourcePaths(
 
 int RunOnSourceFile(std::string home_dir, std::string absolute_file, sjp::JSONObjectIO* json_out)
 {
-	json_out->StartObject("clang tool run");
+	json_out->StartObject("root");
 	
 	const char*  dir_input[3] = {"tool-template", home_dir.c_str(), absolute_file.c_str()};
 	int num_args = 3;
@@ -144,14 +229,15 @@ int RunOnSourceFile(std::string home_dir, std::string absolute_file, sjp::JSONOb
 		RefactoringTool Tool(*Compilations, SourcePaths);
 		ast_matchers::MatchFinder Finder;
 
-		ToolTemplateCallback Callback(json_out);
+		ToolTemplateCallback Callback;
 		DeclarationMatcher decl_match = recordDecl().bind("statementer");
 		Finder.addMatcher(decl_match, &Callback);
+		
+		FrontendActionFactory* factory = newFrontendActionFactory(&Finder);
 
-		json_out->StartArray("method_calls");
-		Tool.run(newFrontendActionFactory(&Finder));
-	
-		json_out->EndCurrent();
+		Tool.run(factory);
+		Callback.DumpAllToJSON(json_out);
+
 	}
 	else
 	{
---
+++ code_to_analyze/sample_streaming_json/test_analysis_source.cpp
@@ -0,0 +1,39 @@
+class Base 
+{
+protected:
+	int mBaseMember;
+};
+
+class Derived1: public Base
+{
+private:
+	int mDerivedMember;
+};
+
+class Derived2: public Derived1
+{
+private:
+	int mDerived2Member;
+};
+
+
+class NotDerived
+{
+private:
+	Base mNotDerivedMember;
+};
+
+class MultiDerived: public Derived1, public NotDerived
+{
+private:
+	int mMultiDerivedMember;
+};
+
+int main(int argc, char** argv)
+{
+	Base a;
+	Derived1 b;
+	Derived2 c;
+	NotDerived d;
+	MultiDerived e;
+}
